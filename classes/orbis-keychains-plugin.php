39f2b3c588ae29d9ab9c92124b91a7d04b5eb928
Remco Tolsma <info@remcotolsma.nl>
2013-02-27 10:37:06 +0100

    Include the http_build_url.php file for backwards compatibility.

--- classes/orbis-keychains-plugin.php
+++ classes/orbis-keychains-plugin.php
@@ -4,9 +4,10 @@ class Orbis_Keychains_Plugin extends Orbis_Plugin {
 	public function __construct( $file ) {
 		parent::__construct( $file );
 
+		$this->plugin_include( 'includes/hosting.php' );
+		$this->plugin_include( 'includes/http_build_url.php' );
 		$this->plugin_include( 'includes/post.php' );
 		$this->plugin_include( 'includes/taxonomy.php' );
-		$this->plugin_include( 'includes/hosting.php' );
 	}
 
 	public function loaded() {
