219aafd35632581ac7094038d1052f7fb505362e
Remco Tolsma <info@remcotolsma.nl>
2014-11-07 09:32:29 +0100

    WordPress Coding Standards optimization:
     20 | ERROR | Expected a sanitizing function (see Codex for 'Data Validation'),
        |       | but instead saw 'get_permalink'

--- templates/hosting-group-keychains.php
+++ templates/hosting-group-keychains.php
@@ -17,7 +17,7 @@ if ( ! empty( $control_panel_keychain_id ) ) : ?>
 					<?php _e( 'Control Panel', 'orbis_keychains' ); ?>
 				</dt>
 				<dd>
-					<a href="<?php echo get_permalink( $control_panel_keychain_id ); ?>">
+					<a href="<?php echo esc_attr( get_permalink( $control_panel_keychain_id ) ); ?>">
 						<?php echo get_the_title( $control_panel_keychain_id ); ?>
 					</a>
 				</dd>
