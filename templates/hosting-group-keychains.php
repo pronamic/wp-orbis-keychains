a755309dc8ee28e232beefb9ca8908bdc95a5beb
Ruben Droogh <rubendroogh@iMac-van-Leo-2.local>
2018-02-27 12:06:34 +0100

    Converted Bootstrap panels to cards.

--- templates/hosting-group-keychains.php
+++ templates/hosting-group-keychains.php
@@ -6,23 +6,24 @@ $control_panel_keychain_id = get_post_meta( $post->ID, '_orbis_hosting_group_con
 
 if ( ! empty( $control_panel_keychain_id ) ) : ?>
 
-	<div class="panel">
-		<header>
-			<h3><?php esc_html_e( 'Keychains', 'orbis_keychains' ); ?></h3>
-		</header>
+	<div class="card">
+		<div class="card-body">
+			<h3 class="card-title"><?php esc_html_e( 'Keychains', 'orbis_keychains' ); ?></h3>
 
-		<div class="content">
-			<dl>
-				<dt>
-					<?php esc_html_e( 'Control Panel', 'orbis_keychains' ); ?>
-				</dt>
-				<dd>
-					<a href="<?php echo esc_attr( get_permalink( $control_panel_keychain_id ) ); ?>">
-						<?php echo get_the_title( $control_panel_keychain_id ); ?>
-					</a>
-				</dd>
-			</dl>
+			<div class="content">
+				<dl>
+					<dt>
+						<?php esc_html_e( 'Control Panel', 'orbis_keychains' ); ?>
+					</dt>
+					<dd>
+						<a href="<?php echo esc_attr( get_permalink( $control_panel_keychain_id ) ); ?>">
+							<?php echo get_the_title( $control_panel_keychain_id ); ?>
+						</a>
+					</dd>
+				</dl>
+			</div>
 		</div>
+		
 	</div>
 
 <?php endif; ?>
