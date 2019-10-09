a3e4c86f3d82d508962f8f6773ff63028bd7edb9
Remco Tolsma <info@remcotolsma.nl>
2019-10-09 15:32:04 +0200

    Disable autocomplete.

--- admin/meta-box-keychain-details.php
+++ admin/meta-box-keychain-details.php
@@ -16,7 +16,7 @@ $email    = get_post_meta( $post->ID, '_orbis_keychain_email', true );
 			<label for="orbis_keychain_url"><?php esc_html_e( 'URL', 'orbis_keychains' ); ?></label>
 		</th>
 		<td>
-			<input id="orbis_keychain_url" name="_orbis_keychain_url" value="<?php echo esc_attr( $url ); ?>" type="url" class="regular-text" />
+			<input id="orbis_keychain_url" name="_orbis_keychain_url" value="<?php echo esc_attr( $url ); ?>" type="url" class="regular-text" autocomplete="off" data-lpignore="true" />
 			<span class="description"><br /><?php echo wp_kses_post( __( 'Use an full URL: for HTTP <code>http://</code>, for FTP <code>ftp://</code>, for SFTP <code>sftp://</code>', 'orbis_keychains' ) ); ?></span>
 		</td>
 	</tr>
@@ -25,7 +25,7 @@ $email    = get_post_meta( $post->ID, '_orbis_keychain_email', true );
 			<label for="orbis_keychain_username"><?php esc_html_e( 'Username', 'orbis_keychains' ); ?></label>
 		</th>
 		<td>
-			<input id="orbis_keychain_username" name="_orbis_keychain_username" value="<?php echo esc_attr( $username ); ?>" type="text" class="regular-text" />
+			<input id="orbis_keychain_username" name="_orbis_keychain_username" value="<?php echo esc_attr( $username ); ?>" type="text" class="regular-text" autocomplete="off" data-lpignore="true" />
 		</td>
 	</tr>
 	<tr valign="top">
@@ -33,7 +33,7 @@ $email    = get_post_meta( $post->ID, '_orbis_keychain_email', true );
 			<label for="orbis_keychain_password"><?php esc_html_e( 'Password', 'orbis_keychains' ); ?></label>
 		</th>
 		<td>
-			<input id="orbis_keychain_password" name="_orbis_keychain_password" value="<?php echo esc_attr( $password ); ?>" type="password" class="regular-text" />
+			<input id="orbis_keychain_password" name="_orbis_keychain_password" value="<?php echo esc_attr( $password ); ?>" type="password" class="regular-text" autocomplete="new-password" data-lpignore="true" />
 		</td>
 	</tr>
 	<tr valign="top">
@@ -41,7 +41,7 @@ $email    = get_post_meta( $post->ID, '_orbis_keychain_email', true );
 			<label for="orbis_keychain_email"><?php esc_html_e( 'E-mail Address', 'orbis_keychains' ); ?></label>
 		</th>
 		<td>
-			<input id="orbis_keychain_email" name="_orbis_keychain_email" value="<?php echo esc_attr( $email ); ?>" type="email" class="regular-text" />
+			<input id="orbis_keychain_email" name="_orbis_keychain_email" value="<?php echo esc_attr( $email ); ?>" type="email" class="regular-text" autocomplete="off" data-lpignore="true" />
 		</td>
 	</tr>
 </table>
